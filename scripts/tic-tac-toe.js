a71db82868809ce684e0efc970444efcee21b8d8
Nolan Gajdascz <78930672+Gajdascz@users.noreply.github.com>
2023-10-19 14:49:51 -0400

    Add player creation and gameboard init logic

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -7,18 +7,14 @@ const gameBoard = (() => {
   for(let i = 0; i < rows; i++) {
     boardArray[i] = [];
     for(let j = 0; j < columns; j++) {
-      boardArray[i].push("cell");
-      console.log(boardArray);
+      boardArray[i].push('null');
     }
   }
-})();
 
-const  displayController = (() => {
+  const getBoard = () => boardArray;
 
-})();
 
-const gameController = (() => {
-
+  return { getBoard };
 })();
 
 // Your players are also going to be stored in objects,
@@ -29,7 +25,27 @@ const createPlayer = (playerName, playerGamePiece) => {
   return { name, gamePiece };
 }
 
+const  displayController = (() => {
+})();
+
+const gameController = (() => {
+ const playerOne = createPlayer(`Nolan`,`X`);
+ const playerTwo = createPlayer(`Kayla`,`O`);
+ console.log(`${playerOne.name}   ${ playerTwo.name}`);
+
+})();
+
+
+
 // and you’re probably going to want an object to control the flow of the game itself.
       // Your main goal here is to have as little global code as possible. 
       // Try tucking everything away inside of a module or factory. 
       // Rule of thumb: if you only ever need ONE of something (gameBoard, displayController), use a module. If you need multiples of something (players!), create them with factories.
+
+
+
+
+      // Game starts, random player is chosen to go first.
+      // chosen first player picks an spot on the gameboard
+        // If the spot is empty, place their game piece. If the spot is taken, do nothing or tell them to choose another space
+      // repeat for second player until there's a win or tie.
